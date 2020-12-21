35ef5bb3a74de412fa6b8a0f28cfd88d604af13a
Skye <1184287060@qq.com>
2020-12-21 16:09:58 +0800

    '****'

--- src/components/content/mainTabBar/MainTabBar.vue
+++ src/components/content/mainTabBar/MainTabBar.vue
@@ -5,23 +5,23 @@
         <!-- <img slot="item-icon" src="@/assets/img/tabbar/home.png" alt="">
         <img slot="item-icon-active" src="@/assets/img/tabbar/home-active.png" alt="">
         <div slot="item-text">首页</div> -->
-        <template v-slot:item-icon><img src="@/assets/img/tabbar/home.png" alt=""></template>
-        <template v-slot:item-icon-active><img src="@/assets/img/tabbar/home-active.png" alt=""></template>
+        <template v-slot:item-icon><img src="assets/img/tabbar/home.png" alt=""></template>
+        <template v-slot:item-icon-active><img src="assets/img/tabbar/home-active.png" alt=""></template>
         <template v-slot:item-text><div>首页</div></template>
       </tab-bar-item>
       <tab-bar-item path="/category">
-        <template v-slot:item-icon><img src="@/assets/img/tabbar/category.png" alt=""></template>
-        <template v-slot:item-icon-active><img src="@/assets/img/tabbar/category-active.png" alt=""></template>
+        <template v-slot:item-icon><img src="assets/img/tabbar/category.png" alt=""></template>
+        <template v-slot:item-icon-active><img src="assets/img/tabbar/category-active.png" alt=""></template>
         <template v-slot:item-text><div>分类</div></template>
       </tab-bar-item>
       <tab-bar-item path="/shopcart">
-        <template v-slot:item-icon><img src="@/assets/img/tabbar/shopcart.png" alt=""></template>
-        <template v-slot:item-icon-active><img src="@/assets/img/tabbar/shopcart-active.png" alt=""></template>
+        <template v-slot:item-icon><img src="assets/img/tabbar/shopcart.png" alt=""></template>
+        <template v-slot:item-icon-active><img src="assets/img/tabbar/shopcart-active.png" alt=""></template>
         <template v-slot:item-text><div>购物车</div></template>
       </tab-bar-item>
       <tab-bar-item path="/profile">
-        <template v-slot:item-icon><img src="@/assets/img/tabbar/profile.png" alt=""></template>
-        <template v-slot:item-icon-active><img src="@/assets/img/tabbar/profile-active.png" alt=""></template>
+        <template v-slot:item-icon><img src="assets/img/tabbar/profile.png" alt=""></template>
+        <template v-slot:item-icon-active><img src="assets/img/tabbar/profile-active.png" alt=""></template>
         <template v-slot:item-text><div>我的</div></template>
       </tab-bar-item>
     </tab-bar>
