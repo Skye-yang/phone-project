bf1ffc6f65cb4f3fb05f0221ee2012cb988bb79c
Skye <1184287060@qq.com>
2021-01-04 17:27:28 +0800

    轮播图

--- src/components/common/swiper/Swiper.vue
+++ src/components/common/swiper/Swiper.vue
@@ -24,10 +24,12 @@ export default {
       type: Number,
       default: 300
     },
+    // 移动比例
     moveRatio: {
       type: Number,
       default: 0.25
     },
+    // 指示器（下面的小圆点）
     showIndicator: {
       type: Boolean,
       default: true
@@ -52,9 +54,9 @@ export default {
     }, 100)
   },
   methods: {
-    /*
-    定时器操作
-    */
+    /**
+     * 定时器操作
+     */
     startTimer() {
       this.playTimer = window.setInterval(() => {
         this.currentIndex++;
@@ -65,9 +67,9 @@ export default {
       window.clearInterval(this.playTimer);
     },
 
-    /*
-    滚动到正确的位置
-    */
+    /**
+     * 滚动到正确的位置
+     */
     scrollContent(currentPosition) {
       // 0.设置正在滚动
       this.scrolling = true;
@@ -83,9 +85,9 @@ export default {
       this.scrolling = false;
     },
 
-    /*
-    校验正确的位置
-    */
+    /**
+     * 校验正确的位置
+     */
     checkPosition() {
       window.setTimeout(() => {
         // 1.校验正确的位置
@@ -103,12 +105,138 @@ export default {
       }, this.animDuration)
     },
 
-    /*
-    设置滚动的位置  轮播图展示 -> 时间到：02：19
-    */
+    /**
+     * 设置滚动的位置
+     */
+    setTransform: function(position){
+      this.swiperStyle.transform = `translate3d(${position}px, 0, 0)`;
+      this.swiperStyle['-webkit-transform'] = `translate3d(${position}px), 0, 0`;
+      this.swiperStyle['-ms-transform'] = `translate3d(${position}px), 0, 0`;
+    },
+
+    /**
+     * 操作DOM，在DOM前后添加slide
+     */
+    handleDom: function(){
+      // 1. 获取要操作的元素
+      let swiperEl = document.querySelector('.swiper');
+      let slidesEls = swiperEl.getElementsByClassName('slide');
+
+      // 2. 保存个数
+      this.slideCount = slidesEls.length;
+
+      // 3. 如果大于1个，那么在前后分别添加一个slide
+      if(this.slideCount > 1) {
+        let cloneFirst = slidesEls[0].cloneNode(true);
+        let cloneLast = slidesEls[this.slideCount - 1].cloneNode(true);
+        swiperEl.insertBefore(cloneLast, slidesEls[0]);
+        swiperEl.appendChild(cloneFirst);
+        this.totalWidth = swiperEl.offsetWidth;
+        this.swiperStyle = swiperEl.style;
+      }
+
+      // 4. 让swiper元素，显示第一个（目前是显示前面添加的最后一个元素）
+      this.setTransform(-this.totalWidth);
+    },
+
+    /**
+     * 拖动事件的处理
+     */
+    touchStart: function(e) {
+      // 1. 如果正在滚动，不可以拖动
+      if(this.scrolling) return;
+
+      // 2. 停止定时器
+      this.stopTimer();
+
+      // 3. 保存开始滚动的位置
+      this.startX = e.touches[0].pageX;
+    },
+    touchMove: function(e) {
+      // 1. 计算出用户拖动的距离
+      this.currentX = e.touches[0].pageX;
+      this.distance = this.currentX - this.startX;
+      let currentPosition = -this.currentIndex * this.totalWidth;
+      let moveDistance = this.distance + currentPosition;
+
+      // 2. 设置当前的位置
+      this.setTransform(moveDistance);
+    },
+
+    touchEnd: function() {
+      // 1. 获取移动的距离
+      let currentMove = Math.abs(this.distance);
+
+      // 2. 判断最终的距离
+      if(this.distance === 0) {
+        return
+      } else if(this.distance > 0 && currentMove > this.totalWidth * this.moveRatio){
+        // 右边移动超过0.5
+        this.currentIndex--;
+      } else if(this.distance < 0 && currentMove > this.totalWidth * this.moveRatio){
+        // 向左移动超过0.5
+        this.currentIndex++;
+      }
+
+      // 3. 移动到正确的位置
+      this.scrollContent(-this.currentIndex * this.totalWidth);
+
+      // 4. 移动完成后重新开启定时器
+      this.startTimer();
+    },
+
+    /**
+     * 控制上一个，下一个
+     */
+    previous: function(){
+      this.changeItem(-1);
+    },
+    next: function(){
+      this.changeItem(1);
+    },
+    changeItem: function(num){
+      // 1. 移除定时器
+      this.stopTimer();
+
+      // 2. 修改index和位置
+      this.currentIndex += num;
+      this.scrollContent(-this.currentIndex * this.totalWidth);
+
+      // 3. 添加定时器
+      this.startTimer();
+    }
+
   }
 }
 </script>
 
 <style>
+#hy-swiper{
+  overflow: hidden;
+  position: relative;
+}
+.swiper{
+  display: flex;
+}
+.indicator{
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  width: 100%;
+  bottom: 8px;
+}
+.indi-item{
+  box-sizing: border-box;
+  width: 8px;
+  height: 8px;
+  border-radius: 4px;
+  background-color: #fff;
+  line-height: 8px;
+  text-align: center;
+  font-size: 12px;
+  margin: 0 5px;
+}
+.indi-item.active{
+  background-color: rgba(212, 62, 46, 1);
+}
 </style>
--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -3,17 +3,27 @@
     <nav-bar class="home-nav">
       <template v-slot:center>购物街</template>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="item in banners" :key="item.acm">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 import NavBar from 'components/common/navbar/NavBar'
 import {getHomeMultidata} from 'network/home'
+import {Swiper, SwiperItem} from 'components/common/swiper'
 
 export default {
   name: "home",
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
   },
   data() {
     return {
